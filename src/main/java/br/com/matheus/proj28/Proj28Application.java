ecf068622a579d6576405f56dc39dbf2ae0eb1b3
Matheus <smatheus799vianna@gmail.com>
2026-09-13 23:06:30 -0300

    Projeto com spring boot

---
+++ src/main/java/br/com/matheus/proj28/Proj28Application.java
@@ -0,0 +1,13 @@
+package br.com.matheus.proj28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj28Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/matheus/proj28/controller/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.matheus.proj28.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Projeto Spring Boot funcionando!";
+    }
+}
